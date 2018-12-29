db4ce8b637ff1c20d0b42536d34a493e26b0ad94
Noahnut <b02204048@ntu.edu.tw>
2018-12-29 17:12:18 +0800

    Update diamond.sol

--- diamond.sol
+++ diamond.sol
@@ -1,31 +1,49 @@
 pragma solidity ^0.5.2;
 
+
+
+contract DiamondAccessControl{
+    
+    mapping(address => bool) internal root_premission;
+    
+    modifier RootPremission(){
+        require(root_premission[msg.sender]);
+        _;
+    }
+    
+    function setThePremission(address newCompany) external RootPremission{
+        require(newCompany != address(0));
+        
+        root_premission[newCompany] = true;
+    }
+    
+    function cancelPremision(address Company) external RootPremission{
+        require(Company != address(0));
+        
+        root_premission[Company] = false;
+    }
+    
+    
+    /*
+    function test_function() external view returns(bool){
+        return(root_premission[msg.sender]);
+    }
+    */
+    
+    
+    
+    
+}
+
 contract ERC721{
     
-    //基於加密貓所使用的ERC721規範
     
-
-
     //function totalSupply() public view returns (uint256 total);
     //function balanceOf(address _owner) public view returns (uint256 balance);
-    //_tokenId == Diamond ID
-    //檢測是否擁有這顆鑽石
     function ownerof(uint256 _tokenId) external view returns (address owner);
-
-    //確認被轉移過去的鑽石是否正確，主要用在transferFrom
-    //_to 鑽石轉移的目標帳戶
     function approve(address _to, uint256 _tokenId) external;
-
-    //轉移鑽石的函數
-    //_to 鑽石轉移的目標帳戶
     function transfer(address _to, uint256 _tokenId) external;
-
-    //在次確認交易
-    //_from 鑽石的原本主人
-    //_to   鑽石的目標帳戶
-    //_tokenId 鑽石ID
     function transferFrom(address _from, address _to, uint256 _tokenId) external;
-    
     
     event Transfer(address from, address to, uint256 tokenId);
     event Approval(address owner, address approved, uint256 tokenId);
@@ -33,12 +51,12 @@ contract ERC721{
     // supportsInterface(bytes4 _interfaceID) external view returns (bool);
 }
 
-contract DiamondBase{
+contract DiamondBase is DiamondAccessControl{
     
-    //基於ERC721的鑽石交換
+    
     event Transfer(address from, address to, uint256 TokenId);
     
-   
+    
     
     struct diamond_profile{
         uint256 diamond_id;
@@ -46,33 +64,30 @@ contract DiamondBase{
         uint diamond_carat;
     }
     
+    struct diamond_id{
+        uint256 diamond_id;
+    }
+    
     diamond_profile[] Diamonds;
     
-    //每個鑽石ID都有一個其對應的主人
-    mapping(uint256 => address) public Diamond_Index_To_Owner;
-
-    //使用者的地址擁有哪些鑽石id
-    mapping(address => uint256) public Owner_To_Diamond_Id;
-
-    //哪些鑽石是要交易時，作為交易人確認用之映射
-    mapping(uint256 => address) public DiamondInder_To_approved;
+    mapping(uint256 => address) public DiamondIndexToOwner;
+    mapping(address => uint256[]) public OwnerToDiamondId;
+    mapping(uint256 => address) public DiamondInderToapproved;
     
-
-    //_from 鑽石原本使用者
-    //_to   鑽石目標帳戶
-    //_tokenId 鑽石ID
-    function _transfer(address _from, address _to, uint256 _tokenId) internal {
-        //此鑽石ID必須是屬於原本使用者
-        require(Diamond_Index_To_Owner[_tokenId] == _from);
+    function _transfer(address _from, address _to, uint256 _tokenId) internal{
+        require(DiamondIndexToOwner[_tokenId] == _from);
         
-        //將此ID所對應的使用者改變
-        Diamond_Index_To_Owner[_tokenId] = _to;
-
-        //觸發事件
+        DiamondIndexToOwner[_tokenId] = _to;
+        for(uint i = 0; i < OwnerToDiamondId[_from].length; i++){
+            if(OwnerToDiamondId[_from][i] == _tokenId){
+                OwnerToDiamondId[_from][i] = 0;
+            }
+        }
+        
+        OwnerToDiamondId[_to].push(_tokenId);
         emit Transfer(_from, _to, _tokenId);
     }
     
-    //
     function _creat_diamond(
         uint256 _diamond_id,
         uint _diamond_price,
@@ -102,15 +117,15 @@ contract DiamondOwnerShip is DiamondBase, ERC721 {
         bytes4(keccak256('transferFrom(address, address, uint256)'));
         
    function _owns(address _claimant, uint256 _tokenId) internal view returns (bool){
-       return Diamond_Index_To_Owner[_tokenId] == _claimant;
+       return DiamondIndexToOwner[_tokenId] == _claimant;
    }
    
    function _approvedFor(address _claimant, uint256 _tokenId) internal view returns (bool){
-       return DiamondInder_To_approved[_tokenId] == _claimant;
+       return DiamondInderToapproved[_tokenId] == _claimant;
    }
    
    function _approve(uint256 _tokenId, address _approved) internal {
-       DiamondInder_To_approved[_tokenId] = _approved;
+       DiamondInderToapproved[_tokenId] = _approved;
    }
    
    function transfer(
@@ -121,7 +136,7 @@ contract DiamondOwnerShip is DiamondBase, ERC721 {
         require(_to != address(0));
         require(_to != address(this));
         
-        require(_owns(msg.sender, _tokenId));
+       
         
         _transfer(msg.sender, _to, _tokenId);
     }
@@ -156,7 +171,7 @@ contract DiamondOwnerShip is DiamondBase, ERC721 {
     function ownerof(uint256 _tokenId)
         external view returns(address owner)
     {
-        owner = Diamond_Index_To_Owner[_tokenId];
+        owner = DiamondIndexToOwner[_tokenId];
         require(owner != address(0));
     }
 }
@@ -164,8 +179,17 @@ contract DiamondOwnerShip is DiamondBase, ERC721 {
 
 contract DiamondCreate is DiamondOwnerShip
 {
-    function creat_diamond(address target_address,uint256 diamond_id,uint _diamond_price, uint _diamond_carat) external{
+    
+    function creat_diamond(address target_address,uint256 diamond_id,uint _diamond_price, uint _diamond_carat) external payable{
+        require(root_premission[msg.sender] == true);
         _creat_diamond(diamond_id,_diamond_price,_diamond_carat, address(this));
+
         _transfer(address(this), target_address, diamond_id);
     }    
+}
+
+contract DiamondCore is DiamondCreate
+{
+    constructor() public {root_premission[msg.sender] = true;}
+    
 }
